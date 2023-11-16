ec5256b767a06be90b161bc8f3e5780cf1c43af6
siddheshc <siddheshc@mkcl.org>
2023-11-16 18:43:34 +0530

    initial commit

---
+++ src/views/RegistrationView.vue
@@ -0,0 +1,420 @@
+<template>
+  <div class="div">
+    <div class="div-2">
+      <div class="div-5">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/9420a804-7a93-43bd-b58f-b98902686e0c?"
+          class="img-2"
+        />
+        <div class="div-6">Create Profile</div>
+      </div>
+    </div>
+    <div class="div-7"><div class="div-8"></div></div>
+    <img
+      loading="lazy"
+      src="https://cdn.builder.io/api/v1/image/assets/TEMP/a61e0e4a-aba2-46e9-b580-0b973e665181?"
+      class="img-3"
+    />
+    <div class="div-9">
+      <div class="div-10">
+        <div class="div-11">+</div>
+        <div class="div-12">Upload photo</div>
+      </div>
+      <div class="div-13">Name *</div>
+      <div class="div-14"></div>
+      <div class="div-15">Birth Date</div>
+      <div class="div-16">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/3d211dec-04e6-41f4-b364-53815def528d?"
+          class="img-4"
+        />
+      </div>
+      <div class="div-17">
+        <div class="div-18">
+          <div class="div-19">Gender</div>
+          <div class="div-20"></div>
+        </div>
+        <div class="div-21">
+          <div class="div-22">Age</div>
+          <div class="div-23"></div>
+        </div>
+      </div>
+      <div class="div-24">Location</div>
+      <div class="div-25">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/11cfe7a5-b263-473e-840e-5baa93742a6e?"
+          class="img-5"
+        />
+      </div>
+      <div class="div-26">Phone Number</div>
+      <div class="div-27"></div>
+      <div class="div-28">Next</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.div {
+  border-radius: 20px;
+  background-color: #fff;
+  display: flex;
+  width: 428px;
+  max-width: 100%;
+  flex-direction: column;
+}
+.div-2 {
+  align-self: stretch;
+  display: flex;
+  margin-top: 28px;
+  width: 100%;
+  flex-direction: column;
+  padding: 0 29px;
+}
+@media (max-width: 991px) {
+  .div-2 {
+    max-width: 100%;
+    padding: 0 20px;
+  }
+}
+.img {
+  aspect-ratio: 4.82;
+  object-fit: contain;
+  object-position: center;
+  width: 82px;
+  overflow: hidden;
+  align-self: stretch;
+  max-width: 100%;
+}
+.div-5 {
+  display: flex;
+  margin-top: 36px;
+  justify-content: space-between;
+  gap: 20px;
+}
+.img-2 {
+  aspect-ratio: 1;
+  object-fit: contain;
+  object-position: center;
+  width: 30px;
+  overflow: hidden;
+  max-width: 100%;
+}
+.div-6 {
+  color: #000;
+  text-align: center;
+  letter-spacing: -0.613px;
+  align-self: center;
+  white-space: nowrap;
+  margin: auto 0;
+  font: 500 24px/102% Poppins, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-6 {
+    white-space: initial;
+  }
+}
+.div-7 {
+  border-radius: 12px;
+  background-color: #d9d9d9;
+  align-self: stretch;
+  display: flex;
+  margin-top: 16px;
+  width: 100%;
+  flex-direction: column;
+  align-items: start;
+}
+@media (max-width: 991px) {
+  .div-7 {
+    max-width: 100%;
+  }
+}
+.div-8 {
+  border-radius: 12px;
+  background-color: #165069;
+  display: flex;
+  width: 90px;
+  height: 10px;
+  flex-direction: column;
+}
+.img-3 {
+  aspect-ratio: 1;
+  object-fit: contain;
+  object-position: center;
+  width: 141px;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  align-self: center;
+  margin-top: 12px;
+  max-width: 100%;
+}
+.div-9 {
+  align-self: stretch;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin: 12px 0 20px;
+  padding: 0 21px;
+}
+@media (max-width: 991px) {
+  .div-9 {
+    padding: 0 20px;
+  }
+}
+.div-10 {
+  border-radius: 6px;
+  background-color: rgba(22, 80, 105, 0.21);
+  align-self: stretch;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  gap: 20px;
+  padding: 18px 121px 18px 91px;
+}
+@media (max-width: 991px) {
+  .div-10 {
+    margin-right: 1px;
+    padding: 0 20px;
+  }
+}
+.div-11 {
+  color: #165069;
+  letter-spacing: -0.649px;
+  margin: auto 0;
+  font: 500 30px/87% Poppins, sans-serif;
+}
+.div-12 {
+  color: #165069;
+  letter-spacing: -0.649px;
+  font: 500 20px/130% Poppins, sans-serif;
+}
+.div-13 {
+  color: #000;
+  letter-spacing: -0.649px;
+  align-self: stretch;
+  margin-top: 17px;
+  white-space: nowrap;
+  font: 500 16px/162% Poppins, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-13 {
+    margin-left: 1px;
+    white-space: initial;
+  }
+}
+.div-14 {
+  border-radius: 8.617px;
+  border: 1px solid #e3e3e3;
+  align-self: stretch;
+  display: flex;
+  margin-top: 13px;
+  width: 100%;
+  height: 50px;
+  flex-direction: column;
+}
+@media (max-width: 991px) {
+  .div-14 {
+    margin-left: 1px;
+  }
+}
+.div-15 {
+  color: #000;
+  letter-spacing: -0.649px;
+  align-self: stretch;
+  margin-top: 10px;
+  white-space: nowrap;
+  font: 500 16px/162% Poppins, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-15 {
+    margin-left: 1px;
+    white-space: initial;
+  }
+}
+.div-16 {
+  border-radius: 8.617px;
+  border: 1px solid #e3e3e3;
+  align-self: stretch;
+  display: flex;
+  margin-top: 13px;
+  width: 100%;
+  flex-direction: column;
+  align-items: end;
+  padding: 13px 20px;
+}
+@media (max-width: 991px) {
+  .div-16 {
+    margin-left: 1px;
+  }
+}
+.img-4 {
+  aspect-ratio: 1.13;
+  object-fit: contain;
+  object-position: center;
+  width: 27px;
+  overflow: hidden;
+  max-width: 100%;
+}
+@media (max-width: 991px) {
+  .img-4 {
+    margin-right: -8px;
+  }
+}
+.div-17 {
+  align-self: stretch;
+  display: flex;
+  margin-top: 19px;
+  justify-content: space-between;
+  gap: 20px;
+}
+@media (max-width: 991px) {
+  .div-17 {
+    margin-left: 1px;
+  }
+}
+.div-18 {
+  display: flex;
+  flex-grow: 1;
+  flex-basis: 0%;
+  flex-direction: column;
+}
+.div-19 {
+  color: #000;
+  letter-spacing: -0.649px;
+  white-space: nowrap;
+  font: 500 16px/162% Poppins, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-19 {
+    white-space: initial;
+  }
+}
+.div-20 {
+  border-radius: 8.617px;
+  border: 1px solid #e3e3e3;
+  display: flex;
+  margin-top: 13px;
+  height: 50px;
+  flex-direction: column;
+}
+.div-21 {
+  display: flex;
+  flex-grow: 1;
+  flex-basis: 0%;
+  flex-direction: column;
+}
+.div-22 {
+  color: #000;
+  letter-spacing: -0.649px;
+  white-space: nowrap;
+  font: 500 16px/162% Poppins, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-22 {
+    white-space: initial;
+  }
+}
+.div-23 {
+  border-radius: 8.617px;
+  border: 1px solid #e3e3e3;
+  display: flex;
+  margin-top: 9px;
+  height: 50px;
+  flex-direction: column;
+}
+.div-24 {
+  color: #000;
+  letter-spacing: -0.649px;
+  align-self: stretch;
+  margin-top: 9px;
+  white-space: nowrap;
+  font: 500 16px/162% Poppins, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-24 {
+    margin-left: 1px;
+    white-space: initial;
+  }
+}
+.div-25 {
+  border-radius: 8.617px;
+  border: 1px solid #e3e3e3;
+  align-self: stretch;
+  display: flex;
+  margin-top: 13px;
+  width: 100%;
+  flex-direction: column;
+  align-items: end;
+  padding: 13px 20px;
+}
+@media (max-width: 991px) {
+  .div-25 {
+    margin-left: 1px;
+  }
+}
+.img-5 {
+  aspect-ratio: 1.13;
+  object-fit: contain;
+  object-position: center;
+  width: 27px;
+  overflow: hidden;
+  max-width: 100%;
+}
+@media (max-width: 991px) {
+  .img-5 {
+    margin-right: -8px;
+  }
+}
+.div-26 {
+  color: #000;
+  letter-spacing: -0.649px;
+  align-self: stretch;
+  margin-top: 9px;
+  white-space: nowrap;
+  font: 500 16px/162% Poppins, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-26 {
+    margin-left: 1px;
+    white-space: initial;
+  }
+}
+.div-27 {
+  border-radius: 8.617px;
+  border: 1px solid #e3e3e3;
+  align-self: stretch;
+  display: flex;
+  margin-top: 13px;
+  width: 100%;
+  height: 50px;
+  flex-direction: column;
+}
+@media (max-width: 991px) {
+  .div-27 {
+    margin-left: 1px;
+  }
+}
+.div-28 {
+  color: #fff;
+  letter-spacing: -0.649px;
+  white-space: nowrap;
+  border-radius: 6px;
+  background-color: #165069;
+  align-self: center;
+  margin-top: 12px;
+  width: 291px;
+  max-width: 100%;
+  align-items: center;
+  padding: 19px 20px;
+  font: 500 21px/125% Poppins, sans-serif;
+}
+@media (max-width: 991px) {
+  .div-28 {
+    white-space: initial;
+  }
+}
+</style>
