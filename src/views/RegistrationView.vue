7fd97fdc5024d7f927ccae3645b48e5e8d6e2358
Siddhesh Chaughule <siddhesh.chaughule21@gmail.com>
2023-12-11 23:54:43 +0530

    registration form design phase 1

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,38 +1,17 @@
 <template>
-  <h2>Create an account</h2>
-  <div class="row mx-auto ml-2">
-    <FormKit type="form" @submit="handleSubmit" class="col-md-6">
-      <FormKit type="text" label="Your Email" name="email" prefix-icon="email" placeholder="email@domain.com"
-        validation="required|email" class="px-0 ml-2"/>
+  <h1 class="my-4">Create an account</h1>
+  <div class="container mx-auto">
+    <FormKit type="form" @submit="handleSubmit">
+      <FormKit type="text" label="Username" name="username" validation="required|matches:/^[a-zA-Z ]*$/|length:5"
+        placeholder="Enter full name" />
+      <FormKit type="radio" label="Gender" placeholder="Select a gender" name="gender"
+        :options="['Male', 'Female', 'Other']" :errors="errors" />
+      <!-- <FormKit type="datepicker" name="pickup" label="Date of Birth" placeholder="DOB" picker-only
+        :max-date="new Date()" /> -->
+      <FormKit type="text" label="Your Email" name="email" placeholder="email@domain.com" validation="required|email" />
     </FormKit>
   </div>
 </template>
-
-<style>
-/* .common-label {
-  margin: 0rem 1rem 0rem;
-  columns: 3;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  color: #2F4F4F;
-}
-input[type=text], select {
-  width: 70%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
-@media only screen and (max-width: 600px) {
-  .common-label {
-    margin: 0rem 1rem 0rem;
-    columns: 3;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    color: #2F4F4F;
-  }
-} */
-</style>
 
 <script>
 export default {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,html}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
   },
   plugins: [],
 };
